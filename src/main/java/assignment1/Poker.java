bce91ec47b79a5f16c2951d9a1c4fb70ed81dd77
Sully Al Shemari <soltanalshemari@cmail.carleton.ca>
2018-09-30 20:11:18 -0400

    added test cases and refactoring
    clean up code and a test cases

--- src/main/java/assignment1/Poker.java
+++ src/main/java/assignment1/Poker.java
@@ -2,11 +2,11 @@ package assignment1;
 import java.io.*; 
 import java.util.*;
 public class Poker 
-{
-
-	public static void main(String args[])  throws FileNotFoundException
-	{
-		String[] cards = new String[50];
+{	
+	String[] cards = new String[50];
+	String[] hand = new String[10];
+	String[] hand2 = new String[10];
+	public void fileReader() throws FileNotFoundException{
 		//file reads for cards then converts scanner to string and splits 
 		//the whitespace in the string
 		try{
@@ -25,5 +25,50 @@ public class Poker
 			System.out.println("this is " +  i + " card " + cards[i]);
 		}
 	}
+	
+	public String[] getCards(){
+		return hand;
+	}
+	
+	public String[] getAICards(){
+		return hand2;
+	}
+	
+	public boolean addCard(String[] cards){
+		
+		int i;
+		for( i = 0; i<5; i++){
+			hand[i]=cards[i];
+		}
+		if(i==5){
+			Arrays.sort(hand);
+			System.out.println(hand);
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean addCard2(String[] cards){
+		int i;
+		for( i = 4; i<10; i++){
+			hand[i]=cards[i];
+		}
+		if(i ==10){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean flush(String[] cards){
+		char suit = cards[0].charAt(0);
+	}
+	
+	public static void main(String args[])  throws FileNotFoundException
+	{
+		
+	}
 }
-
--- src/test/java/assignment1/PokerTest.java
+++ src/test/java/assignment1/PokerTest.java
@@ -3,61 +3,57 @@ package assignment1;
 import junit.framework.TestCase;
 public class PokerTest extends TestCase{
 	
-	Poker hand = new Poker;
-	Poker hand2 = new Poker;
+	Poker hand = new Poker();
 	
-	public void testHand(){
-		
-	    assertEquals(true, hand.equals(hand));
-		
-	}
-	
-	
-	public void testDealCards(Poker hand, Poker hand2){
-	    assertEquals(true, hand.add());
-	    assertEquals(true, hand2.add());
+	public void testDealCards(){
+	    assertEquals(true, hand.addCard(hand.getCards()));
+	    assertEquals(true, hand.addCard2(hand.getAICards()));
 
 	}
 	
-	public void hasHighCard(Poker hand, Poker hand2){
-		assertEquals(true, hand.highCard());
-		assertEquals(true, hand2.highCard());
+	public void testHasHighCard(){
+		assertEquals(true, hand.highCard(hand.getCards()));
+		assertEquals(true, hand.highCard(hand.getAICards()));
 	}
 	
-    public void hasPair(Poker hand, Poker hand2){
-		assertEquals(true, hand.pair());
-		assertEquals(true, hand2.pair());
+    public void testHasPair(){
+		assertEquals(true, hand.pair(hand.getCards()));
+		assertEquals(true, hand.pair(hand.getAICards()));
 	}
 
-    public void hasTwoPair(Poker hand, Poker hand2){
+    public void testHasTwoPair(){
+    	assertEquals(true, hand.twoPairs());
+    	assertEquals(true, hand.twoPairs());
+    }
+
+    public void testHasThreeOfAKind(){
+    	assertEquals(true, hand.threeOfKind());
+    	assertEquals(true, hand.threeOfKind());
+    }
+
+    public void testHasStraight(){
+    	assertEquals(true, hand.straight());
+    	assertEquals(true, hand.straight());
+    }
+
+    public void testHasFlush(){
+    	assertEquals(true, hand.flush());
+    	assertEquals(true, hand.flush());
+    }
+
+    public void testHasFullHouse(){
     	
     }
 
-    public void hasThreeOfAKind(Poker hand){
+    public void testHasFourOfAKind(){
     	
     }
 
-    public void hasStraight(Poker hand){
-    	
-    }
-
-    public void hasFlush(Poker hand){
-    	
-    }
-
-    public void hasFullHouse(Poker hand){
-    	
-    }
-
-    public void hasFourOfAKind(Poker hand){
-    	
-    }
-
-    public void hasStraightFlush(Poker hand){
+    public void testHasStraightFlush(){
     	
     }
     
-    public void hasRoyalFlush(Poker hand){
+    public void testHasRoyalFlush(){
     	
     }
 	
